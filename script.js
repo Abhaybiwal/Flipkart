641a863956008819bc4963f3fc12225441f3ad7f
abhay biwal <abhaybiwal2@gmail.com>
2022-12-03 07:46:36 -0500

    code

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const changeMode = () => {
+    let mybody = document.body;
+    // let obj = document.getElementsByClassName('main');
+
+    mybody.classList.toggle('mydark')
+}
+
+
+function loadCoupon(){
+    document.getElementById("coupon").style.visibility = "visible";
+    document.getElementById("main").style.opacity="0.2";
+}
+
+function closeCoupon(){
+    document.getElementById("coupon").style.visibility = "hidden";
+    document.getElementById("main").style.opacity="1";
+}
+
+
+
+
 const header = document.querySelector('.Header');
 const nav = document.querySelector('.Navbar')
 const topOfNav = header.offsetTop;
@@ -12,3 +33,6 @@ function fixNav(){
 }
 // for fixing the navbar at top
 window.addEventListener('scroll',fixNav);
+
+
+
